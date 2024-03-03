11d06572b15155d8ea7e17980ceb2f4d814d1502
Vishnu <majumdarvishnu268@gmail.com>
2024-03-03 20:42:30 +0530

    Daa Lab

--- src/com/DaaLab/PrimMST.java
+++ src/com/DaaLab/PrimMST.java
@@ -1,12 +1,13 @@
 package com.DaaLab;
 
-import java.util.Arrays;
+public class PrimMST {
+    // Number of vertices in the graph
+    static final int V = 5;
 
-class MST {
-
-    private static final int V = 5;
-
-    int minKey(int key[], Boolean mstSet[]) {
+    // A utility function to find the vertex with
+    // minimum key value, from the set of vertices
+    // not yet included in MST
+    static int minKey(int key[], Boolean mstSet[]) {
         int min = Integer.MAX_VALUE, min_index = -1;
 
         for (int v = 0; v < V; v++)
@@ -18,13 +19,18 @@ class MST {
         return min_index;
     }
 
-    void printMST(int parent[], int graph[][]) {
+    // A utility function to print the
+    // constructed MST stored in parent[]
+    static void printMST(int parent[], int graph[][]) {
         System.out.println("Edge \tWeight");
         for (int i = 1; i < V; i++)
             System.out.println(parent[i] + " - " + i + "\t" + graph[i][parent[i]]);
     }
 
-    void primMST(int graph[][]) {
+    // Function to construct and print MST for
+    // a graph represented using adjacency
+    // matrix representation
+    static void primMST(int graph[][]) {
         int parent[] = new int[V];
         int key[] = new int[V];
         Boolean mstSet[] = new Boolean[V];
@@ -37,12 +43,11 @@ class MST {
         key[0] = 0;
         parent[0] = -1;
 
-        long startTime = System.nanoTime();
+        long startTime = System.currentTimeMillis();
 
         for (int count = 0; count < V - 1; count++) {
             int u = minKey(key, mstSet);
             mstSet[u] = true;
-
             for (int v = 0; v < V; v++)
                 if (graph[u][v] != 0 && mstSet[v] == false && graph[u][v] < key[v]) {
                     parent[v] = u;
@@ -50,21 +55,24 @@ class MST {
                 }
         }
 
-        long endTime = System.nanoTime();
+        long endTime = System.currentTimeMillis();
         long executionTime = endTime - startTime;
+        System.out.println("Execution time: " + executionTime + " milliseconds");
 
         printMST(parent, graph);
-        System.out.println("Execution time: " + executionTime + " nanoseconds");
     }
 
     public static void main(String[] args) {
-        MST t = new MST();
-        int graph[][] = new int[][] { { 0, 2, 0, 6, 0 },
-                { 2, 0, 3, 8, 5 },
-                { 0, 3, 0, 0, 7 },
-                { 6, 8, 0, 0, 9 },
-                { 0, 5, 7, 9, 0 } };
+        int graph[][] = {
+                {0, 2, 0, 6, 0},
+                {2, 0, 3, 8, 5},
+                {0, 3, 0, 0, 7},
+                {6, 8, 0, 0, 9},
+                {0, 5, 7, 9, 0}
+        };
 
-        t.primMST(graph);
+        // Print the solution
+        primMST(graph);
     }
 }
+
